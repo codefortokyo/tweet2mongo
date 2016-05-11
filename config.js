cd6c34d478b7c1d953f5bf12364b158f5acddaa6
osoken <osoken.devel@outlook.jp>
2016-05-12 01:23:41 +0900

    add DBNAME, DBPORT

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ var config = {
   'twitter_consumer_secret': process.env['TWITTER_CONSUMER_SECRET'],
   'twitter_access_token_key': process.env['TWITTER_ACCESS_TOKEN_KEY'],
   'twitter_access_token_secret': process.env['TWITTER_ACCESS_TOKEN_SECRET'],
-  'track_word': process.env['TWEET_STREAM_TRACK_WORD']
+  'track_word': process.env['TWEET_STREAM_TRACK_WORD'],
+  'dbname': process.env['TWEET_STREAM_DBNAME'],
+  'dbport': process.env['TWEET_STREAM_DBPORT']
 };
 
 try
