9aaf66a9ccc322e3d55a7d1302461ca141c0b7c0
osoken <osoken.devel@outlook.jp>
2016-05-12 00:39:54 +0900

    use config

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 
 var twitter = require('twitter');
 
-var twit = new twitter({
-  consumer_key: 'xx',
-  consumer_secret: 'xx',
-  access_token_key: 'xx',
-  access_token_secret: 'xx'
+var config = require('./config');
+
+var t = new twitter({
+  consumer_key: config.twitter_consumer_key,
+  consumer_secret: config.twitter_consumer_secret,
+  access_token_key: config.twitter_access_token_key,
+  access_token_secret: config.twitter_access_token_secret
 });
 
 var keyword = process.argv[2];
 
 var option = {'track': keyword};
 
-twit.stream('statuses/filter', option, function(stream) {
+t.stream('statuses/filter', option, function(stream) {
   stream.on('data', function (data) {
     console.log(data);
   });
