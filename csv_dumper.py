9d4ff04fa112ab208344dc98e2f76fd65336af0b
osoken <osoken.devel@outlook.jp>
2016-05-17 23:24:56 +0900

    changed timestamp format

--- csv_dumper.py
+++ csv_dumper.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys, json, csv
+from datetime import datetime
 
 from shapely.geometry import shape, mapping
 
@@ -8,6 +9,9 @@ def pointify(d):
   if 'coordinates' in d and d['coordinates'] is not None:
     return mapping(shape(d['coordinates']).centroid)['coordinates']
   return mapping(shape(d['place']['bounding_box']).centroid)['coordinates']
+
+def ts2dt(ts):
+  return datetime.fromtimestamp(int(ts) / 1000).replace(microsecond=int(ts) % 1000 * 1000)
 
 def map_encode(d):
   return map(lambda x: x.encode('utf-8') if isinstance(x, unicode) else x, d)
@@ -17,4 +21,4 @@ if __name__ == '__main__':
   w.writerow(['time', 'latitude', 'longitude', 'lang', 'text', 'username'])
   for d in json.loads(sys.stdin.read()):
     p = pointify(d)
-    w.writerow(map_encode((str(d['timestamp_ms']), str(p[1]), str(p[0]), d['lang'], d['text'], d['user']['screen_name'])))
+    w.writerow(map_encode((ts2dt(d['timestamp_ms']).strftime('%Y-%m-%dT%H:%M:%S.%fZ').replace('000Z','Z'), str(p[1]), str(p[0]), d['lang'], d['text'].replace('\n',''), d['user']['screen_name'])))
