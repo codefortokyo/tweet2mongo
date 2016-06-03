25af94c3e3c5876c375b9308e5082d5d5dbf0cba
osoken <osoken.devel@outlook.jp>
2016-06-04 01:23:40 +0900

    tidy routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 var db = require('../db');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
 });
 
-router.get('/api/tweet', function(req, res, next) {
+router.get('/api/tweet', function(req, res) {
   var timestamp_ms = req.query.timestamp_ms || new Date().getTime();
   var limit = req.query.limit || 10;
   var skip = req.query.skip || 0;
@@ -16,7 +16,7 @@ router.get('/api/tweet', function(req, res, next) {
     {limit:limit,skip:skip,sort:{'timestamp_ms':-1}},
     function(err, data) {
       if (err != null) {
-        return res.status(400).send(e);
+        return res.status(400).send(err);
       }
       return res.json(data);
     }
